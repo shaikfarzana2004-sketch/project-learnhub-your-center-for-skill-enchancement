d69fd9a4520d0fe6236ad1ba941d230e828c656d
shaikfarzana2004-sketch <shaikfarzana2004@gmail.com>
2026-02-21 13:37:29 +0530

    Add files via upload

---
+++ project Files/backend/config/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectionOfDb = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "video-course-application",
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Could not connect to MongoDB:", err);
+      process.exit(1);
+    });
+};
+
+module.exports = connectionOfDb;
